30adbe71278f51796d531ec28d536ecdfb8f6219
Alexfeynman2023 <alexfeynman2023@gmail.com>
2025-06-04 19:07:02 -0600

    create sign-in and sign-out UI

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@/components/ui/card";
+
+const Page = () => {
+    return (
+        <div >
+            <Card>
+                Sign up page
+            </Card>
+        </div>
+    );
+}
+
+export default Page;
